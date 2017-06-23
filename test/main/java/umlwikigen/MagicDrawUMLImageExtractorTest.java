e58ed533c53a23abbac06ffcd407912a36762d99
Abdol-Hamid <kkabdolh@ndc.nasa.gov>
2017-06-23 08:54:13 -0400

    Added test classes

---
+++ test/main/java/umlwikigen/MagicDrawUMLImageExtractorTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+/**
+ * Author: Kareem Abdol-Hamid kkabdolh
+ * Version: 6/23/2017
+ */
+public class MagicDrawUMLImageExtractorTest extends TestCase {
+
+}
---
+++ test/main/java/umlwikigen/SharePointRestTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+/**
+ * Author: Kareem Abdol-Hamid kkabdolh
+ * Version: 6/23/2017
+ */
+public class SharePointRestTest extends TestCase {
+
+}
