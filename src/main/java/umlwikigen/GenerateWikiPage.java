da0b70cf9a940f297523c6c26a6b68611949f956
Abdol-Hamid <kkabdolh@ndc.nasa.gov>
2017-06-23 08:42:08 -0400

    Updated File structure and added GenerateWikiPage

---
+++ src/main/java/umlwikigen/GenerateWikiPage.java
@@ -0,0 +1,24 @@
+package umlwikigen;
+
+import java.io.File;
+
+/**
+ * Author: Kareem Abdol-Hamid kkabdolh
+ * Version: 6/23/2017
+ */
+public class GenerateWikiPage {
+    private String wikiPage;
+    private String imgDest;
+
+    public GenerateWikiPage(String imgDest) {
+        this.wikiPage = "";
+        this.imgDest = imgDest;
+        generate();
+    }
+
+    private void generate() {
+        File dir = new File(imgDest);
+        File[] dirList = dir.listFiles();
+        //wikiPage += "<img src=\"" + img.getAbsoluteFile() + "/><br/>";
+    }
+}
